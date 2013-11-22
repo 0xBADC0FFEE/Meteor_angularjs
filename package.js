5687f2aaed12f96e88119b1700ced0d2bb2bd342
vawerv <vawerv@gmail.com>
2013-11-22 08:36:14 +0400

    add files to meteor project

--- package.js
+++ package.js
@@ -9,7 +9,14 @@ Package.on_use(function (api) {
 
   api.use('webapp', ['server']);
 
-  api.add_files('angular.js', 'client');
+  api.add_files('angular.min.js', 'client');
+  api.add_files('angular-animate.min.js', 'client');
+  api.add_files('angular-cookies.min.js', 'client');
+  api.add_files('angular-loader.min.js', 'client');
+  api.add_files('angular-resource.min.js', 'client');
+  api.add_files('angular-route.min.js', 'client');
+  api.add_files('angular-sanitize.min.js', 'client');
+  api.add_files('angular-touch.min.js', 'client');
   api.add_files('client.js', 'client');
  
   api.add_files('server.js', 'server');
